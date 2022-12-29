b64a9348057b97b2c524b317ef4b4e2154233dcb
hdsk1234 <59996778+hdsk1234@users.noreply.github.com>
2022-12-29 16:58:43 +0900

    Update 스택.cc

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,3 +1,9 @@
+/*
+시간복잡도: O(n)
+공간복잡도: O(n)
+어려웠던 점: 예외처리를 안 하면 런타임에러가 나서 짜증났다
+*/
+
 #include <iostream>
 #include <string>
 #include <stack>
@@ -31,7 +37,7 @@ int main(){
             int temp = 0;
             int digit = pow(10, t.size()-1);
             for(int j = 0; j < t.size(); j++){
-                temp += int(t[j] - 48) * digit;
+                temp += int(t[j] - 48) * digit; // 0의 아스키코드 : 48
                 digit /= 10;
             }
             stack.push(temp);
