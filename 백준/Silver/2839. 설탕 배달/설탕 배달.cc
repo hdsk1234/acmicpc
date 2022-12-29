cf19bca1bf8bae4094d073a33d2f8b704931eda1
hdsk1234 <59996778+hdsk1234@users.noreply.github.com>
2022-12-29 16:53:00 +0900

    Update 설탕 배달.cc

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,9 +1,17 @@
+/*
+시간복잡도: O(n)
+공간복잡도: O(1)
+
+어려웠던 점: dp는 그냥 생소해서 어려웠다
+*/
+
 #include<stdio.h>
 
 int main(){
 	int num, sum = 0, min = -1;
 	scanf("%d", &num);
 	
+	// 3의 개수가 늘어나면서 되는 5의 개수를 각각 세서 최솟값 구한다. 근데 이게 dp냐..?
 	for(int i = 0; i <= num; i += 3){
 		if((num - i) % 5 == 0){
 			sum = i/3 + (num - i)/5;
