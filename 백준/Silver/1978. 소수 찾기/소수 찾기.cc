dc4809254c418500a8646be7b97467194be102c7
hdsk1234 <59996778+hdsk1234@users.noreply.github.com>
2022-12-29 16:49:09 +0900

    Update 소수 찾기.cc

--- 백준/Silver/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.cc
@@ -4,14 +4,21 @@ int main(void){
 	
 	int N, i, temp;
 	int prime = 0;
-	scanf("%d", &N);
+	scanf("%d", &N); //숫자 개수
 	int num[N];
 	
 	
+	//숫자 입력
 	for(i = 0; i < N; i++){
 		scanf("%d", &num[i]);
 	}	
 	
+	//그냥 무식하게
+	//나누어떨어질 때까지 2로 나눈다
+	//안 나눠떨어지면 3으로 반복
+	//4로반복
+	//5로반복
+	//....
 	for(i = 0; i < N; i++){
 		temp = 2;
 		while(temp < num[i]){
